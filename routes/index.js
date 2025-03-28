b9b2aa0e2f7c7bfdf61af569d5e3a6a981fa27ed
Sridhar Sing <sridharsing14@gmail.com>
2025-03-28 17:10:06 +0530

    fix isue

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,7 @@
 const { Router } = require("express");
 const router = Router();
 
-router.get("/", async (req, res) => {
-    return res.send(`<h1>Letter Drive server is running.</h1>`);
-});
+router.get("/", require("./home"));
 router.use("/api/auth", require("./authRoutes"));
 router.use("/api/letters", require("./letterRoutes"));
 
